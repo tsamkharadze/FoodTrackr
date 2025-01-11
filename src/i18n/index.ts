8befc9f00ba5ac7b5864bb1fd2bf565f585f564a
animeshveliani <meshveliani.ani@gmail.com>
2025-01-11 14:25:55 +0400

    add languagedetector

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,6 +1,7 @@
 import i18n from "i18next";
-
 import { initReactI18next } from "react-i18next";
+import LngDetector from "i18next-browser-languagedetector";
+
 import headerEn from "./en/header.json";
 import headerKa from "./ka/header.json";
 import loginKa from "./ka/login.json";
@@ -12,8 +13,18 @@ import profileEn from "./en/profile.json";
 import ErrorEn from "./en/errors.json";
 import ErrorKa from "./ka/errors.json";
 
+// Initialize language detector before i18next
+const languageDetector = new LngDetector();
+languageDetector.init({
+  order: ["localStorage", "navigator"],
+  lookupLocalStorage: "i18nextLng",
+  caches: ["localStorage"],
+  checkWhitelist: true,
+});
+
 i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(languageDetector)
+  .use(initReactI18next)
   .init({
     resources: {
       ka: {
@@ -35,10 +46,21 @@ i18n
         },
       },
     },
-    lng: "ka",
     fallbackLng: "en",
-
+    supportedLngs: ["ka", "en"],
+    detection: {
+      order: ["localStorage", "navigator"],
+      caches: ["localStorage"],
+    },
     interpolation: {
       escapeValue: false,
     },
   });
+
+// Handle language changes
+i18n.on("languageChanged", (lng) => {
+  document.documentElement.lang = lng;
+  localStorage.setItem("i18nextLng", lng);
+});
+
+export default i18n;
